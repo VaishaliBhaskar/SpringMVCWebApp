130ed6dc9a5d32bc0fabb0ad842465afa26cd97f
Vaishali Bhaskar <vaishalibhaskar2015@gmail.com>
2023-05-13 09:48:47 +0530

    service class modified

--- src/main/java/com/practiceapp/project/Service/PhoneBookService.java
+++ src/main/java/com/practiceapp/project/Service/PhoneBookService.java
@@ -14,33 +14,39 @@ public class PhoneBookService {
 	@Autowired
 	private PhoneBookRepository phone;
 	
-	
+	//create a single contact
 	public Phonebook createContact(Phonebook book) {
 		phone.save(book);
 		return book;
 	}
 		
+	//create multiple contacts
 	public List<Phonebook> createAllContact(List<Phonebook> books){
 		return phone.saveAll(books);
 	}
 	
-	
+	//get contact by id
 	public Phonebook getContactById(int id) {
 		return phone.findById(id).get();
 	
 	}
-	
+	//get all contacts
 	public List<Phonebook> getAllContact() {
 		return phone.findAll();
 	}
+	
+	//delete contact by id
 	public void deleteContactById(int id) {
 		 phone.deleteById(id);
 		 
 	}
+	
+	//delete all contacts
 	public void deleteAllContact() {
 		phone.deleteAll();
 	}
 	
+	//update a contact
 	public Phonebook updateContact(Phonebook book) {
 		Phonebook Existing=phone.findById(book.getId()).orElse(null); 
 		Existing.setName(book.getName());
