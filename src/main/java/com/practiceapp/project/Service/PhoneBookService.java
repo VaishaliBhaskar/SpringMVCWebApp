8a653923e439d4e0fef17ae5e1a2656812afaba9
Vaishali Bhaskar <vaishalibhaskar2015@gmail.com>
2023-05-13 10:19:42 +0530

    commnets added

--- src/main/java/com/practiceapp/project/Service/PhoneBookService.java
+++ src/main/java/com/practiceapp/project/Service/PhoneBookService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PhoneBookService {
 
-	@Autowired
+	@Autowired //enable dependency injection
 	private PhoneBookRepository phone;
 	
 	//create a single contact
